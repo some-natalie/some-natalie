5bddf988b5bec499b6b0ef03ea3c62b35804aa5d
Natalie Somersall <natalie.somersall@gmail.com>
2023-04-19 08:51:34 -0600

    Microservices fiddling (#8)
    * try this
    
    * try this
    
    * move script
    
    * add reminder
    
    * move reminder to oncreate
    
    * move reminder back to postcreate

--- load-workspace.sh
+++ load-workspace.sh
@@ -4,7 +4,7 @@ set -e
 # Get the list of other repositories from devcontainer.json using jq
 REPOS=$(jq -r '.customizations.codespaces.repositories' .devcontainer/devcontainer.json | jq -r 'keys[]')
 
-# Clone the other repos
+# Open the other repos
 for repo in $REPOS; do
     repo_name=$(echo "$repo" | cut -d'/' -f2) # split the repo name from owner
     code-insiders --add /workspaces/"$repo_name"

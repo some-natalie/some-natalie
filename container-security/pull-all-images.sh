cb61db161a3272e5513c009bee9fef0224571a9d
Natalie Somersall <natalie.somersall@gmail.com>
2025-08-04 20:14:19 -0600

    add image pull list

---
+++ container-security/pull-all-images.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# This script pulls all images for the container escapes workshop.
+
+images=(
+    "docker.io/library/ubuntu:24.04"
+    "docker.io/redhat/ubi9:9.6"
+    "ghcr.io/some-natalie/some-natalie/whoami:latest"
+    "cgr.dev/chainguard/python:latest"
+    "cgr.dev/chainguard/curl:latest"
+    "docker.io/library/nginx:latest"
+    "ghcr.io/some-natalie/some-natalie/cowsay:latest"
+    "ghcr.io/some-natalie/some-natalie/command-injection:latest"
+    "ghcr.io/some-natalie/some-natalie/command-injection-noshell:latest"
+    "ghcr.io/some-natalie/some-natalie/command-injection-noshell-noroot:latest"
+)
+
+for image in "${images[@]}"; do
+    echo "Pulling $image..."
+    docker pull "$image"
+done
